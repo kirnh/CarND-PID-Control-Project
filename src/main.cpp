971b6a762b363b7025eaccb731c70e836d0b160b
Kiran Hegde <kiran.hegde121@gmail.com>
2017-09-07 11:02:45 +0530

    Vehicles runs around the track without leaving the permitted surface. PID params were manually tuned, satisfies submission requirements. However, still need to update README with reflections on tuning procedure. Also, might want to try twiddle for param optimization and a controller for speed to achieve better timing around the track.

--- src/main.cpp
+++ src/main.cpp
@@ -34,9 +34,9 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  double init_Kp = -0.5;
-  double init_Ki = 0;
-  double init_Kd = -1.0;
+  double init_Kp = -0.15;
+  double init_Ki = -0.0005;
+  double init_Kd = -0.9;
   pid.Init(init_Kp, init_Ki, init_Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
